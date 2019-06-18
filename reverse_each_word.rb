a4df4504d8aa7a01c9caeae53f6fd4673facfbf8
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-18 06:18:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,14 @@ def reverse_each_word(sentence)
   reversed = []
   arr_sentence = sentence.split(' ')
   
-  arr_sentence.each {|word| reversed << word.reverse!}
+  arr_sentence.each {|word| reversed << word.reverse}
   print reversed.join(' ')
 end
 
 # def reverse_each_word(sentence)
 #   arr_sentence = sentence.split(' ')
   
-#   reversed = arr_sentence.collect {|word| word.reverse!}
+#   reversed = arr_sentence.collect {|word| word.reverse}
   
 #   print reversed.join(' ')
 # end
