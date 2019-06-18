1c461bb58a251fd53f557e2c095c534759126509
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-18 06:12:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,3 @@
-# def reverse_each_word(sentence)
-#   reverse_sentence = []
-  
-#   arr_sentence = sentence.split(' ')
-#   print arr_sentence
-# end
-
-# sentence = "Hello there, and how are you?"
-
-# puts reverse_each_word(sentence)
-
-
-
 def reverse_each_word(sentence)
   reversed_arr = []
   arr_sentence = sentence.split(' ')
@@ -18,7 +5,3 @@ def reverse_each_word(sentence)
   arr_sentence.each {|word| reversed_arr << word.reverse!}
   print reversed_arr.join(' ')
 end
-
-sentence = "Hello there, and how are you?"
-
-puts reverse_each_word(sentence)
